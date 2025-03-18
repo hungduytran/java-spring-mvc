e80fb8f9ee272ef99c731f15bf11e1de6e2a2dc1
Tran Duy Hung <duyhung6604@gmail.com>
2025-03-18 22:29:17 +0700

    54

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,39 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import vn.hoidanit.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        return "hello";
+    }
+}
+
+//@RestController
+//public class UserController {
+//
+//    private UserService userService;
+//
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("")
+//    public String getHomePage() {
+//        return this.userService.handleHello();
+//    }
+//}
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -3,5 +3,8 @@ package vn.hoidanit.laptopshop.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyService {
+public class UserService {
+    public String handleHello() {
+        return "Hello World!";
+    }
 }
